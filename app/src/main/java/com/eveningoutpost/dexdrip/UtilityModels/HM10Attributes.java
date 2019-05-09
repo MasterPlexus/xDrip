f9639f3dec9987a68fb6ab9ade22710d856c4c8c
MasterPlexus <dirkwindhorst@googlemail.com>
2019-05-09 19:39:21 +0200

    set my pin for sweetreader... shit test hardcore

--- app/src/main/java/com/eveningoutpost/dexdrip/UtilityModels/HM10Attributes.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/UtilityModels/HM10Attributes.java
@@ -7,7 +7,7 @@ public class HM10Attributes {
     public static final String CLIENT_CHARACTERISTIC_CONFIG = "00002902-0000-1000-8000-00805f9b34fb";
     public static final String HM_10_SERVICE = "0000ffe0-0000-1000-8000-00805f9b34fb";
     public static final String HM_RX_TX = "0000ffe1-0000-1000-8000-00805f9b34fb";
-    public static final String HM_DEFAULT_BT_PIN = "000000";
+    public static final String HM_DEFAULT_BT_PIN = "852373"; //todo add pinrequest also for sweetreader
 
     // Experimental support for "Transmiter PL" from Marek Macner @FPV-UAV
     public static final String TRANSMITER_PL_SERVICE = "c97433f0-be8f-4dc8-b6f0-5343e6100eb4";
