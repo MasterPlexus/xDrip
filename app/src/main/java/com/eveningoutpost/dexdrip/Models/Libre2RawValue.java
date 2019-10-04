61dfa8f4798bc0429adc4303b74f37a2ad9bec8b
MasterPlexus <dirkwindhorst@googlemail.com>
2019-10-04 22:03:41 +0200

    Libre2_Status: add Class

--- app/src/main/java/com/eveningoutpost/dexdrip/Models/Libre2RawValue.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/Models/Libre2RawValue.java
@@ -9,9 +9,11 @@ package com.eveningoutpost.dexdrip.Models;
         import com.activeandroid.annotation.Table;
         import com.activeandroid.query.Select;
 
+        import java.sql.Array;
         import java.sql.ResultSet;
         import java.sql.ResultSetMetaData;
         import java.sql.SQLException;
+        import java.util.ArrayList;
         import java.util.Date;
         import java.util.List;
 
@@ -60,16 +62,18 @@ public class Libre2RawValue extends PlusModel {
         UserError.Log.d("Libre2RawValue", "start read LibreSensors");
         try {
             UserError.Log.d("Libre2RawValue", "start query");
-            ResultSet rs = (ResultSet) new Select("serial, MIN(ts) as ts_from, MAX(ts) AS ts_to")
-                    .from(Libre2RawValue.class)
+            List<Libre2Sensor> rs = new Select("serial, MIN(ts) as ts_from, MAX(ts) AS ts_to")
+                    .from(Libre2Sensor.class)
                     .groupBy("serial")
                     .orderBy("ts ASC")
                     .limit(10)
                     .execute();
             UserError.Log.d("Libre2RawValue", "start while");
-            while (rs.next()) {
+            for (Libre2Sensor rsSensor : rs ) {
+
+
                 UserError.Log.d("Libre2RawValue", "start sum");
-                Sum += rs.getString("serial") + " from: " + DateFormat.format("dd.MM.yyyy",rs.getLong("ts_from")) + " to: " + DateFormat.format("dd.MM.yyyy",rs.getLong("ts_to")) +"\n";
+                Sum += rsSensor.serial + " from: " + DateFormat.format("dd.MM.yyyy",rsSensor.ts_from) + " to: " + DateFormat.format("dd.MM.yyyy",rsSensor.ts_to) +"\n";
             }
 
         } catch (Exception e) {
@@ -82,4 +86,17 @@ public class Libre2RawValue extends PlusModel {
     public static void updateDB() {
         fixUpTable(schema, false);
     }
+}
+
+class Libre2Sensor extends PlusModel {
+
+    @Column(name = "serial", index = true)
+    public String serial;
+
+    @Column(name = "ts_from", index = true)
+    public long ts_from;
+
+    @Column(name = "ts_to", index = true)
+    public long ts_to;
+
 }
