17258e34951b85656c3457c9a34268c328adbcd3
MasterPlexus <dirkwindhorst@googlemail.com>
2019-10-06 19:23:35 +0200

    Libre2_Status: perzent

--- app/src/main/java/com/eveningoutpost/dexdrip/Models/Libre2Sensor.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/Models/Libre2Sensor.java
@@ -45,7 +45,7 @@ public class Libre2Sensor extends PlusModel {
                     "\n" + DateFormat.format("dd.MM.yy",Sensorpart.ts_from) +
                     " to: " + DateFormat.format("dd.MM.yy",Sensorpart.ts_to) +
                     " (" +JoH.niceTimeScalarShortWithDecimalHours(Sensorpart.ts_to - Sensorpart.ts_from) + ")" +
-                    " readings: " + ((Sensorpart.readings /((Sensorpart.ts_to - Sensorpart.ts_from)))/600) + "%\n" +
+                    " readings: " + (((Sensorpart.readings *100)/((Sensorpart.ts_to - Sensorpart.ts_from)))/600) + "%\n" +
                     "------------------\n";
         }
         return Sum;
